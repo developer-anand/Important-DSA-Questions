6a10ccc1f0c7aa4420048e08954d2a185eeb4531
developer-anand <61963289+developer-anand@users.noreply.github.com>
2020-05-04 23:18:06 +0530

    day_18.cpp

--- day_18.cpp
+++ day_18.cpp
@@ -1,47 +1,41 @@
-#include<iostream>
+#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
-
-void spiral_print(int a[][1000],int m,int n){
-            int sta_row =0;
-            int sta_col = 0;
-            int end_row = m-1;
-            int end_col = n-1;
-
-
-            //Print it
-            while(sta_row<=end_row and sta_col<=end_col){
-                    //Print first ROW
-                        for(int i=sta_col; i<=end_col; i++){
-                        cout<<a[sta_row][i]<<" ";
-                        }sta_row++;
-                    //For last coloumn
-                        for(int i=sta_row; i<=end_row; i++){
-                            cout<<a[i][end_col]<<" ";
-                        }   end_col--;
-                    //For last row
-                        for(int i=end_col; i>=sta_col; i--){
-                            cout<<a[end_row][i]<<" ";
-                            }end_row--;
-                    //Start coloumn
-
-                        for(int i=end_row; i<=sta_row; i--){
-                            cout<<a[i][sta_col]<<" ";
-                            }  sta_col++;
-}
-}
-int main(){
-    int a[1000][1000]={0};
-    int m,n;
-    cin>>m>>n;
-
-    int val=1;
-    for(int row=0; row<m; row++){
-        for(int col=0; col<n; col++){
-            a[row][col] = val;
-            val=val+1;
-            cout<<a[row][col]<<",";
-        }cout<<endl;
+void readMatrix(int a[][10],int R,int C){
+    for(int i=0;i<R;i++){
+        for(int j=0;j<C;j++){
+            cin>>a[i][j];
+        }
     }
-    spiral_print(a,m,n);
-return 0;
+}
+void spiralPrint(int a[][10],int R,int C){
+    int startRow=0,endRow=R-1,startCol=0,endCol=C-1;
+   while (startRow<=endRow && startCol <=endCol){
+    for(int i=startRow;i<=endRow;i++){
+        cout<<a[i][startCol]<<", ";
+    }
+    startCol++;
+   for(int j=startCol;j<=endCol;j++) {
+       cout<<a[endRow][j]<<", ";
+   }
+   endRow--;
+if(endCol>=startCol){
+   for(int i=endRow;i>=startRow;i--){
+       cout<<a[i][endCol]<<", ";
+   }
+   endCol--;}
+   if(startRow<=endRow) {
+          for(int j=endCol;j>=startCol;j--){
+          cout<<a[startRow][j]<<", ";
+       }
+      startRow++;}
+}}
+int main() {
+    int a[10][10];
+    int R,C;
+    cin>>R>>C;
+    readMatrix(a,R,C);
+    spiralPrint(a,R,C);
+    cout<<"END";
+    return 0;
 }
