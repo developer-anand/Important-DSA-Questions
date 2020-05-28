ff3a8a479900faa85d961f1a22edab3bbc3b25cb
developer-anand <imcoderaj@gmail.com>
2020-05-28 17:25:34 +0530

    Add files via upload

---
+++ day_33.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n;
+	cin>>n;
+
+	int a[100];
+	for(int i=0;i<n;i++) {
+		cin>>a[i];
+	}
+
+	//generate all subarray
+	for(int i=0; i<n; i++) {
+		for(int j=i; j<n; j++) {
+			for(int k=i; k<=j; k++) {
+				cout<<a[k]<<",";
+			}
+			cout<<endl;
+		}
+	}
+}
