b69ea04568cec0b919c17091c5d05a15d026f13d
developer-anand <cyberaj123@gmail.com>
2020-05-29 18:02:00 +0530

    Update day_35.cpp

--- day_35.cpp
+++ day_35.cpp
@@ -1,4 +1,11 @@
+/*Two pointers is really an easy and effective technique which is typically used for searching pairs in a sorted array.
+
+Given a sorted array A (sorted in ascending order), having N integers, find if there exists any pair of elements (A[i], A[j]) such that their sum is equal to X.
 //Given a sorted Array, Find Pair of Elements that sum to k (Given)
+
+How does this work?
+The algorithm basically uses the fact that the input array is sorted. We start the sum of extreme values (smallest and largest) and conditionally move both pointers. We move left pointer i when the sum of A[i] and A[j] is less than X. We do not miss any pair because the sum is already smaller than X. Same logic applies for right pointer j.
+*/
 #include<iostream>
 using namespace std;
 
