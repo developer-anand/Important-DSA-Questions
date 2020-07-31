9a5b1aff75c9e87f21276ea27186f85052d9b223
developer-anand <cyberaj123@gmail.com>
2020-07-31 23:08:34 +0530

    Update day_42_flaaping_game.cpp

--- day_42_flaaping_game.cpp
+++ day_42_flaaping_game.cpp
@@ -24,3 +24,30 @@ int main() {
   }
 }
 
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  int arr[n];
+  int ones = 0;
+  for (int i = 0; i < n; i++) {
+    cin >> arr[i];
+    ones += arr[i];
+    arr[i] = arr[i] == 1 ? -1 : 1;
+  }
+
+  if (ones == n)
+    cout << n - 1;
+  else {
+    int sum = 0, ans = 0;
+    for (int i = 0; i < n; i++) {
+      sum = max(0, sum + arr[i]);
+      ans = max(sum, ans);
+    }
+    cout << (ones + ans);
+  }
+}
+
+
